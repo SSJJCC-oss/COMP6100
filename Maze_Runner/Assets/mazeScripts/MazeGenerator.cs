3fffcf39a2714b0c689f1760f36db0ae688f7ce6
Leon <leonmorin410@gmail.com>
2022-12-09 16:12:20 +0000

    commenting

--- Maze_Runner/Assets/mazeScripts/MazeGenerator.cs
+++ Maze_Runner/Assets/mazeScripts/MazeGenerator.cs
@@ -18,7 +18,6 @@ public class MazeGenerator : MonoBehaviour
     private void Start()
     {
         GenerateMaze(mazeSize);
-        //StartCoroutine(slowGenerateMaze(mazeSize));
         surface.BuildNavMesh();
         //spawn zombies
         StartCoroutine("spawning");
@@ -26,6 +25,7 @@ public class MazeGenerator : MonoBehaviour
 
     private void Update()
     {
+        //set position of maze and navmesh surface
         gameObject.transform.position = new Vector3(21.9f, -1.2f, 22);
         surface.transform.position = new Vector3(21.9f, -2.7f, 22);
         
@@ -113,7 +113,7 @@ public class MazeGenerator : MonoBehaviour
                         currentPath[currentPath.Count - 1].RemoveWall(0);//remove wall right of current node
                         break;
                     case 2:
-                        chosenNode.RemoveWall(0);//remove righ of chosen
+                        chosenNode.RemoveWall(0);//remove right of chosen
                         currentPath[currentPath.Count - 1].RemoveWall(1);//remove left of current
                         break;
                     case 3:
@@ -127,133 +127,15 @@ public class MazeGenerator : MonoBehaviour
                 }
 
                 currentPath.Add(chosenNode);
-                //chosenNode.SetState(NodeState.Current);
             }
             else
             {
                 //backtrack to more possible nodes
                 //adding current node to completed list
                 CompletedNodes.Add(currentPath[currentPath.Count-1]);
-                //currentPath[currentPath.Count-1].SetState(NodeState.Completed);
                 ///removing node from current path
                 currentPath.RemoveAt(currentPath.Count-1);
             }
-        }
-    }
-
-    IEnumerator slowGenerateMaze(Vector2Int size)
-    {
-        List<MazeNode> nodes = new List<MazeNode>();
-
-        // Create nodes
-        for (int x = 0; x < size.x; x++)
-        {
-            for (int y = 0; y < size.y; y++)
-            {
-                Vector3 nodePos = new Vector3(x - (size.x / 2f), 0, y - (size.y / 2f))*nodeSize;
-                MazeNode newNode = Instantiate(nodePrefab, nodePos, Quaternion.identity, transform);
-                nodes.Add(newNode);
-
-                yield return null;
-            }
-        }
-
-        List<MazeNode> currentPath = new List<MazeNode>();
-        List<MazeNode> completedNodes = new List<MazeNode>();
-
-        // Choose starting node
-        currentPath.Add(nodes[Random.Range(0, nodes.Count)]);
-        currentPath[0].SetState(NodeState.Current);
-
-        while (completedNodes.Count < nodes.Count)
-        {
-            // Check nodes next to the current node
-            List<int> possibleNextNodes = new List<int>();
-            List<int> possibleDirections = new List<int>();
-
-            int currentNodeIndex = nodes.IndexOf(currentPath[currentPath.Count - 1]);
-            int currentNodeX = currentNodeIndex / size.y;
-            int currentNodeY = currentNodeIndex % size.y;
-
-            if (currentNodeX < size.x - 1)
-            {
-                // Check node to the right of the current node
-                if (!completedNodes.Contains(nodes[currentNodeIndex + size.y]) &&
-                    !currentPath.Contains(nodes[currentNodeIndex + size.y]))
-                {
-                    possibleDirections.Add(1);
-                    possibleNextNodes.Add(currentNodeIndex + size.y);
-                }
-            }
-            if (currentNodeX > 0)
-            {
-                // Check node to the left of the current node
-                if (!completedNodes.Contains(nodes[currentNodeIndex - size.y]) &&
-                    !currentPath.Contains(nodes[currentNodeIndex - size.y]))
-                {
-                    possibleDirections.Add(2);
-                    possibleNextNodes.Add(currentNodeIndex - size.y);
-                }
-            }
-            if (currentNodeY < size.y - 1)
-            {
-                // Check node above the current node
-                if (!completedNodes.Contains(nodes[currentNodeIndex + 1]) &&
-                    !currentPath.Contains(nodes[currentNodeIndex + 1]))
-                {
-                    possibleDirections.Add(3);
-                    possibleNextNodes.Add(currentNodeIndex + 1);
-                }
-            }
-            if (currentNodeY > 0)
-            {
-                // Check node below the current node
-                if (!completedNodes.Contains(nodes[currentNodeIndex - 1]) &&
-                    !currentPath.Contains(nodes[currentNodeIndex - 1]))
-                {
-                    possibleDirections.Add(4);
-                    possibleNextNodes.Add(currentNodeIndex - 1);
-                }
-            }
-
-            // Choose next node
-            if (possibleDirections.Count > 0)
-            {
-                int chosenDirection = Random.Range(0, possibleDirections.Count);
-                MazeNode chosenNode = nodes[possibleNextNodes[chosenDirection]];
-
-                switch (possibleDirections[chosenDirection])
-                {
-                    case 1:
-                        chosenNode.RemoveWall(1);
-                        currentPath[currentPath.Count - 1].RemoveWall(0);
-                        break;
-                    case 2:
-                        chosenNode.RemoveWall(0);
-                        currentPath[currentPath.Count - 1].RemoveWall(1);
-                        break;
-                    case 3:
-                        chosenNode.RemoveWall(3);
-                        currentPath[currentPath.Count - 1].RemoveWall(2);
-                        break;
-                    case 4:
-                        chosenNode.RemoveWall(2);
-                        currentPath[currentPath.Count - 1].RemoveWall(3);
-                        break;
-                }
-
-                currentPath.Add(chosenNode);
-                chosenNode.SetState(NodeState.Current);
-            }
-            else
-            {
-                completedNodes.Add(currentPath[currentPath.Count - 1]);
-
-                currentPath[currentPath.Count - 1].SetState(NodeState.Completed);
-                currentPath.RemoveAt(currentPath.Count - 1);
-            }
-
-            yield return new WaitForSeconds(0.05f);
         }
     }
 
@@ -261,7 +143,7 @@ public class MazeGenerator : MonoBehaviour
     {
             //wait time to spawn
             yield return new WaitForSeconds(spawnTime);
-            //instantiate zombies
+            //instantiate zombies at specific position add them to enemy list
             if (enemyList.Count < maxEnemy)
             {
                 Vector3 pos = new Vector3(29.25f, -1, 29.42f);
